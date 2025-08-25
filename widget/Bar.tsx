45fe93dbd873b22b7038858b49b4b7f73235d1f4
Lawjay Lee <lawjaylee1@icloud.com>
2025-08-25 16:00:11 -0700

    Use AstalNetwork instead of nmcli

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -2,9 +2,10 @@ import app from "ags/gtk4/app"
 import { Astal, Gtk, Gdk } from "ags/gtk4"
 import { createPoll } from "ags/time"
 import GLib from "gi://GLib?version=2.0"
-import { createBinding, createState } from "gnim"
+import { createBinding, createState, For, With } from "gnim"
 import AstalWp from "gi://AstalWp"
 import AstalBattery from "gi://AstalBattery"
+import AstalNetwork from "gi://AstalNetwork"
 
 function BatteryWidget() {
   const battery = AstalBattery.get_default()
@@ -71,20 +72,61 @@ function VolumeWidget() {
 }
 
 function NetworkWidget() {
-  const networkStateIcon = createPoll(
-    "",
-    1000 * 5,
-    ["nmcli", "--fields", "STATE", "device", "status"],
-    (stdout: string, _) => {
-      const status = stdout.split("\n")[1].trim()
-      if (status === "connected") {
-        return "network-wireless-signal-excellent-symbolic"
-      } else {
-        return "network-wireless-offline-symbolic"
-      }
-    },
+  const network = AstalNetwork.get_default()
+  const wifi = createBinding(network, "wifi")
+
+  const sorted = (arr: Array<AstalNetwork.AccessPoint>) => {
+    return arr.filter((ap) => !!ap.ssid).sort((a, b) => b.strength - a.strength)
+  }
+
+  async function connect(ap: AstalNetwork.AccessPoint) {
+    // connecting to ap is not yet supported
+    // https://github.com/Aylur/astal/pull/13
+    try {
+      await execAsync(`nmcli d wifi connect ${ap.bssid}`)
+    } catch (error) {
+      // you can implement a popup asking for password here
+      console.error(error)
+    }
+  }
+
+  return (
+    <box visible={wifi(Boolean)} orientation={Gtk.Orientation.VERTICAL}>
+      <With value={wifi}>
+        {(wifi) =>
+          wifi && (
+            <menubutton>
+              <image
+                iconName={createBinding(wifi, "iconName")}
+                iconSize={Gtk.IconSize.LARGE}
+              />
+              <popover>
+                <box orientation={Gtk.Orientation.VERTICAL}>
+                  <For each={createBinding(wifi, "accessPoints")(sorted)}>
+                    {(ap: AstalNetwork.AccessPoint) => (
+                      <button onClicked={() => connect(ap)}>
+                        <box spacing={4}>
+                          <image iconName={createBinding(ap, "iconName")} />
+                          <label label={createBinding(ap, "ssid")} />
+                          <image
+                            iconName="object-select-symbolic"
+                            visible={createBinding(
+                              wifi,
+                              "activeAccessPoint",
+                            )((active) => active === ap)}
+                          />
+                        </box>
+                      </button>
+                    )}
+                  </For>
+                </box>
+              </popover>
+            </menubutton>
+          )
+        }
+      </With>
+    </box>
   )
-  return <Gtk.Image iconName={networkStateIcon} iconSize={Gtk.IconSize.LARGE} />
 }
 
 function DateTimeWidget() {
